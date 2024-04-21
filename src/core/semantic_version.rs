24a01da567545188e5180ea1268fd2e520ec6d73
Bartłomiej Sacharski <beton@cementownia.org>
2024-04-21 12:07:35 +0200

    feat: add logic for applying convention commits to semantic version

--- src/core/semantic_version.rs
+++ src/core/semantic_version.rs
@@ -1,4 +1,4 @@
-use crate::core::conventional_commit::ConventionalCommit;
+use crate::core::conventional_commit::{CommitType, ConventionalCommit};
 
 #[derive(Debug, PartialEq)]
 struct SemanticVersion {
@@ -19,14 +19,22 @@ impl SemanticVersion {
     }
 
     pub fn apply_commit(self, commit: ConventionalCommit) -> SemanticVersion {
-        todo!("Implement method for applying commits and thus incrementing version")
+        if commit.is_breaking_change {
+            return Self::new(self.major + 1, self.minor, self.patch, self.metadata);
+        }
+
+        match commit.commit_type {
+            CommitType::Feat => Self::new(self.major, self.minor + 1, self.patch, self.metadata),
+            CommitType::Fix => Self::new(self.major, self.minor, self.patch + 1, self.metadata),
+            CommitType::Custom(_) => Self::new(self.major, self.minor, self.patch, self.metadata),
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::core::conventional_commit::CommitType::Fix;
-    use crate::core::conventional_commit::ConventionalCommit;
+    use crate::core::conventional_commit::CommitType::{Feat, Fix};
+    use crate::core::conventional_commit::{CommitType, ConventionalCommit};
     use crate::core::semantic_version::SemanticVersion;
 
     #[test]
@@ -47,5 +55,65 @@ mod tests {
 
         // then
         assert_eq!(new_version, SemanticVersion::new(2, 0, 0, None));
+    }
+
+    #[test]
+    fn should_increase_minor_version_when_introducing_new_feature() {
+        // given
+        let version = SemanticVersion::new(1, 0, 0, None);
+        let commit = ConventionalCommit {
+            commit_type: Feat,
+            is_breaking_change: false,
+            description: String::from("Some new feature"),
+            body: None,
+            footer: None,
+            scopes: None,
+        };
+
+        // when
+        let new_version = version.apply_commit(commit);
+
+        // then
+        assert_eq!(new_version, SemanticVersion::new(1, 1, 0, None));
+    }
+
+    #[test]
+    fn should_increase_minor_version_when_introducing_a_fix() {
+        // given
+        let version = SemanticVersion::new(1, 0, 0, None);
+        let commit = ConventionalCommit {
+            commit_type: Fix,
+            is_breaking_change: false,
+            description: String::from("Some bugfix"),
+            body: None,
+            footer: None,
+            scopes: None,
+        };
+
+        // when
+        let new_version = version.apply_commit(commit);
+
+        // then
+        assert_eq!(new_version, SemanticVersion::new(1, 0, 1, None));
+    }
+
+    #[test]
+    fn should_keep_version_as_it_was_when_applying_custom_type_of_commit() {
+        // given
+        let version = SemanticVersion::new(1, 0, 0, None);
+        let commit = ConventionalCommit {
+            commit_type: CommitType::Custom(String::from("docs")),
+            is_breaking_change: false,
+            description: String::from("Updated documentation"),
+            body: None,
+            footer: None,
+            scopes: None,
+        };
+
+        // when
+        let new_version = version.apply_commit(commit);
+
+        // then
+        assert_eq!(new_version, SemanticVersion::new(1, 0, 0, None));
     }
 }
