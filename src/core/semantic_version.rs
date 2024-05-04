a065f9f2ec598ad098642fae28791f68fd8280fa
Bartłomiej Sacharski <beton@cementownia.org>
2024-05-04 10:59:14 +0200

    feat: add PreReleaseType parsing from string

--- src/core/semantic_version.rs
+++ src/core/semantic_version.rs
@@ -18,6 +18,17 @@ enum PreReleaseType {
     RC,
 }
 
+impl PreReleaseType {
+    pub fn from(str: &str) -> Option<PreReleaseType> {
+        match str {
+            "alpha" => Some(PreReleaseType::Alpha),
+            "beta" => Some(PreReleaseType::Beta),
+            "rc" => Some(PreReleaseType::RC),
+            _ => None,
+        }
+    }
+}
+
 impl PartialEq for PreReleaseType {
     fn eq(&self, other: &Self) -> bool {
         self == other
@@ -186,7 +197,7 @@ mod tests {
     use crate::core::conventional_commit::CommitType::{Feat, Fix};
     use crate::core::conventional_commit::{CommitType, ConventionalCommit};
     use crate::core::semantic_version::PreReleaseType::{Alpha, Beta, RC};
-    use crate::core::semantic_version::{PreRelease, SemanticVersion};
+    use crate::core::semantic_version::{PreRelease, PreReleaseType, SemanticVersion};
 
     #[test]
     fn should_increase_major_version_when_introducing_breaking_change() {
@@ -447,5 +458,28 @@ mod tests {
     #[test]
     fn should_consider_rc_as_greater_than_alpha() {
         assert!(RC > Alpha)
+    }
+
+    #[test]
+    fn should_convert_alpha_string_to_alpha_enum() {
+        assert_eq!(
+            PreReleaseType::from("alpha").unwrap(),
+            PreReleaseType::Alpha
+        )
+    }
+
+    #[test]
+    fn should_convert_beta_string_to_beta_enum() {
+        assert_eq!(PreReleaseType::from("beta").unwrap(), PreReleaseType::Beta)
+    }
+
+    #[test]
+    fn should_convert_rc_string_to_rc_enum() {
+        assert_eq!(PreReleaseType::from("rc").unwrap(), PreReleaseType::RC)
+    }
+
+    #[test]
+    fn should_return_none_when_trying_to_convert_unknown_value() {
+        assert_eq!(PreReleaseType::from("rcc"), None)
     }
 }
